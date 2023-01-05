f5b915358bf7bab462c48eb1e2da4ef90ce4b333
KristinWilliams <wkristin71@yahoo.co.uk>
2023-01-04 18:31:31 -0600

    changed ssl in database

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,14 +1,14 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
       ssl: {
-        rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
+        ca: env("DATABASE_CA"),
       },
     },
     debug: false,
